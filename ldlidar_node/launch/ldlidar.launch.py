1eb43c2511719ecf46be7fc1295aad3881366768
Can Altineller <altineller@gmail.com>
2024-08-15 20:11:45 +0300

    namespace test

--- ldlidar_node/launch/ldlidar.launch.py
+++ ldlidar_node/launch/ldlidar.launch.py
@@ -30,44 +30,16 @@ def generate_launch_description():
         package='ldlidar_node',
         executable='ldlidar_node',
         name=node_name,
-        namespace='',
+        namespace='/',
         output='screen',
         parameters=[
             lidar_config_path  # Parameters
         ]
     )
 
-    # URDF path
-    '''
-    urdf_file_name = 'ldlidar_descr.urdf.xml'
-    urdf = os.path.join(
-        get_package_share_directory('ldlidar_node'),
-        'urdf',
-        urdf_file_name)
-    with open(urdf, 'r') as infp:
-        robot_desc = infp.read()
-
-    # Robot State Publisher node
-    rsp_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='ldlidar_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_desc}],
-        arguments=[urdf]
-    )
-    '''
-
     # Define LaunchDescription variable
     ld = LaunchDescription()
-
-    # Launch arguments
     ld.add_action(declare_node_name_cmd)
-
-    # Launch Nav2 Lifecycle Manager
-    ##ld.add_action(rsp_node)
-
-    # LDLidar Lifecycle node
     ld.add_action(ldlidar_node)
 
     return ld
